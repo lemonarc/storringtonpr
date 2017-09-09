44f4040747bd2a31d072921ffe176991bd554af4
Leon Aves <leonaves1@gmail.com>
2017-09-09 17:15:25 +0100

    Update our-team page.

--- www/js/index.js
+++ www/js/index.js
@@ -2,22 +2,24 @@ const Flickity = require('flickity');
 
 const carousel = document.querySelector('.hero_splash');
 
-const flickCarousel = new Flickity(carousel, {
-  // options
-  wrapAround: true,
-  draggable: true,
-  setGallerySize: false,
-  autoPlay: 4000,
-  selectedAttraction: 0.05,
-  friction: 0.5
-});
+if (carousel) {
+  const flickCarousel = new Flickity(carousel, {
+    // options
+    wrapAround: true,
+    draggable: true,
+    setGallerySize: false,
+    autoPlay: 4000,
+    selectedAttraction: 0.05,
+    friction: 0.5
+  });
 
-flickCarousel.on('scroll', function(event, progress) {
-  flickCarousel.slides.forEach(function(slide, i) {
-    slide.cells.forEach(function(cell) {
-      const heroInformation = cell.element.querySelector('.hero_information');
-      const transform = ((progress - slide.x) / 2) * -1;
-      heroInformation.style.transform = 'translateX(' + transform.toString() + 'px)';
+  flickCarousel.on('scroll', function(event, progress) {
+    flickCarousel.slides.forEach(function(slide, i) {
+      slide.cells.forEach(function(cell) {
+        const heroInformation = cell.element.querySelector('.hero_information');
+        const transform = ((progress - slide.x) / 2) * -1;
+        heroInformation.style.transform = 'translateX(' + transform.toString() + 'px)';
+      });
     });
   });
-});
+}
